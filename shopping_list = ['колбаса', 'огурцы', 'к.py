b06c68fdacd62b9befd04df397110c9f4deea12a
zasutsky <zasutsky_aleksey@mail.ru>
2022-07-21 12:39:35 +0500

    Initial commit

---
+++ shopping_list = ['колбаса', 'огурцы', 'к.py
@@ -0,0 +1,5 @@
+shopping_list = ['колбаса', 'огурцы', 'картошка', 'лук', 'яйцо', 'квас']
+print('Список продуктов для окрошки:')
+for products in shopping_list:
+    print(products)
+print('Приятного аппетита!')
